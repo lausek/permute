d5b14b7443e659e364e4ced1655325a50caa4e6b
lausek <root@lausek.eu>
2022-04-01 22:05:05 +0200

    add cli

--- permute/__main__.py
+++ permute/__main__.py
@@ -1,5 +1,56 @@
+from argparse import ArgumentParser
+from permute.permute import Permute
+
+
+def build_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "--min",
+        dest="min_len",
+        type=int,
+        default=0,
+        help="only output words with at least MIN_LEN chars",
+    )
+    parser.add_argument(
+        "--max",
+        dest="max_len",
+        type=int,
+        default=None,
+        help="only output words with at most MAX_LEN chars",
+    )
+
+    parser.add_argument(
+        "-c", "--with-capital", dest="with_capital", action="store_true", default=False
+    )
+    parser.add_argument(
+        "-l",
+        "--with-lowercase",
+        dest="with_lowercase",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "-u",
+        "--with-uppercase",
+        dest="with_uppercase",
+        action="store_true",
+        default=False,
+    )
+
+    return parser
+
+
 def main():
-    pass
+    import sys
+
+    parser = build_parser()
+    args = parser.parse_args()
+    words = list(word.strip() for word in sys.stdin.readlines() if word.strip())
+    permute = Permute(words=words, **vars(args))
+
+    for word in permute.create_generator():
+        print(word)
 
 
 if __name__ == "__main__":
